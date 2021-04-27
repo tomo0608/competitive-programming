1f3a2f727384ea89acdc271be3deebfcdeb9be86
sugiyama tomohiro <totatari0608@gmail.com>
2021-04-27 21:29:44 +0900

    add getangle

--- geometry.cpp
+++ geometry.cpp
@@ -23,11 +23,26 @@ struct Point{
         return {x/k, y/k};
     }
 
-    Point operator*(const Point &p){
+    double operator*(const Point &p){
         return x*p.x + y*p.y;
     }
 
-    Point operator^(const Point &p){
+    double operator^(const Point &p){
         return x*p.y - y*p.x;
     }
+
+    double getangle(){
+        // 偏角を度数で求める(0 <= angle <= 360)
+        if(y >= 0.0){
+            double I = x / sqrt((*this)*(*this));
+            double angle = acos(I) * 180.0 / 3.14159265358979;
+            return angle;
+        }
+        else{
+            double I = x / sqrt((*this)*(*this));
+            double angle = acos(I) * 180.0 / 3.14159265358979;
+            return 360.0 - angle;
+        }
+    }
 };
+
